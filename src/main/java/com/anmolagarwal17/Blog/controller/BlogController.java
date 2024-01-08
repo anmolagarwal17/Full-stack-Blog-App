5fc0487a9a5884fa8d8ce95cf3668f4225cc2d0e
Anmol Agarwal <anmolagarwal170@gmail.com>
2024-01-08 22:24:32 +0530

    Added delete blog API

--- src/main/java/com/anmolagarwal17/Blog/controller/BlogController.java
+++ src/main/java/com/anmolagarwal17/Blog/controller/BlogController.java
@@ -3,7 +3,19 @@ package com.anmolagarwal17.Blog.controller;
 import com.anmolagarwal17.Blog.pojo.BlogDTO;
 import com.anmolagarwal17.Blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+<<<<<<< Updated upstream
 import org.springframework.web.bind.annotation.*;
+=======
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+>>>>>>> Stashed changes
 
 import java.util.List;
 
@@ -15,29 +27,45 @@ public class BlogController {
     BlogService blogService;
 
     @GetMapping("/{id}")
+<<<<<<< Updated upstream
     @CrossOrigin(origins = "http://localhost:5173") // Allow requests from the specified origin
     public BlogDTO getBlogById(@PathVariable(required = true) long id){
+=======
+    public BlogDTO getBlogById(@PathVariable(required = true) long id) {
+>>>>>>> Stashed changes
         return blogService.getBlogById(id);
     }
 
     @PostMapping
+<<<<<<< Updated upstream
     @CrossOrigin(origins = "http://localhost:5173") // Allow requests from the specified origin
     public BlogDTO createBlog(@RequestBody BlogDTO blogDTO){
+=======
+    public BlogDTO createBlog(@RequestBody BlogDTO blogDTO) {
+>>>>>>> Stashed changes
         // TODO: 30-12-2023 check if any user is logged in, if yes then save 
         //  algo ensure that id should be null or else return error
         return blogService.createBlog(blogDTO);
     }
-    
+
     @PutMapping
+<<<<<<< Updated upstream
     @CrossOrigin(origins = "http://localhost:5173") // Allow requests from the specified origin
     public BlogDTO updateBlog(@RequestBody BlogDTO blogDTO){
+=======
+    public BlogDTO updateBlog(@RequestBody BlogDTO blogDTO) {
+>>>>>>> Stashed changes
         // TODO: 30-12-2023 check if logged use is the owner of blog id(getBlogByIdAndCreatedBy, if yes then only edit
         return blogService.updateBlog(blogDTO);
     }
-    
+
     @GetMapping("/search")
+<<<<<<< Updated upstream
     @CrossOrigin(origins = "http://localhost:5173") // Allow requests from the specified origin
     public List<BlogDTO> searchBlog(@RequestParam(required = true) String value){
+=======
+    public List<BlogDTO> searchBlog(@RequestParam(required = true) String value) {
+>>>>>>> Stashed changes
         return blogService.searchBlog(value);
         // TODO: 30-12-2023 add pagination later 
         // TODO: 30-12-2023 read some blog about how to add pagination in java spring boot application (dev.to, medium etc)
@@ -51,5 +79,10 @@ public class BlogController {
 //        void returnNoFavicon() {
 //        }
 //    }
+
+    @DeleteMapping("/{id}")
+    public void deleteBlog(@PathVariable(required = true) long id) {
+        blogService.deleteBlog(id);
+    }
 }
 
